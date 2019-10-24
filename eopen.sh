5b0948e5af97b6f0c47440c7556cb6684a6b3dad
Koichi Nakashima <koichi@nksm.name>
2019-10-25 01:37:14 +0900

    eopen: Add --version

--- eopen.sh
+++ eopen.sh
@@ -2,6 +2,8 @@
 
 set -eu
 
+VERSION=0.1.0
+
 [ $# -eq 0 ] && set -- .
 
 abort() {
@@ -29,18 +31,19 @@ is_protocol() {
 
 usage() {
   cat <<'HERE'
-Usage: eopen [-e | -n] [file | directory | protocol]
+Usage: eopen [options] [file | directory | uri]
 
-examples
-  eopen                     # Open the current directory in explorer.
-  eopen /etc/               # Open the specified directory in explorer.
-  eopen -n /etc/            # Open the specified directory in new instance of explorer.
-  eopen ~/image.jpg         # Open the file in associated application.
-  eopen -e /etc/hosts       # Open the file in text editor ($EOPEN_EDITOR).
-  eopen http://google.com   # Open the url in default browser.
+options:
+  -e, --editor      Open the file in text editor ($EOPEN_EDITOR)
+  -n, --new         Open the specified directory in new instance of explorer
+  -v, --version     Display the version
+  -h, --help        You're looking at it
 
-  The path of file or directory allows linux and windows path.
+note:
+  The file or the directory allows linux and windows path.
   (e.g. /etc/hosts, C:/Windows/System32/drivers/etc/hosts)
+
+  The uri must start with protocol schema. (e.g http:, https:)
 HERE
 exit
 }
@@ -52,34 +55,49 @@ for arg; do
     -e | --editor) EDITOR=1 ;;
     -n | --new) NEW=1 ;;
     -h | --help) usage ;;
+    -v | --version) echo "$VERSION"; exit ;;
     -?*) abort "unrecognized option '$arg'" ;;
     *) set -- "$@" "$arg"
   esac
   shift
 done
 
-if [ "$EDITOR" ]; then
+open_editor() {
   if [ ! "${EOPEN_EDITOR:-}" ]; then
     # shellcheck disable=SC2016
     abort 'Environment variable $EOPEN_EDITOR is not set'
   fi
 
   if [ $# -gt 0 ]; then
-    tmp=$(wslpath -aw "$1")
-    set -- "$tmp"
+    winpath=$(wslpath -aw "$1")
+    shift
+    set -- "$winpath" "$@"
   fi
   "$EOPEN_EDITOR" "$@"
-else
+}
+
+open_explorer() {
   if is_winpath "$1"; then
     set -- "file://$1"
   elif ! is_protocol "$1"; then
     [ -e "$1" ] || abort "'$1': No such file or directory"
-    tmp=$(wslpath -aw "$1")
-    set -- "$tmp"
+    winpath=$(wslpath -aw "$1")
+    shift
+    set -- "$winpath" "$@"
   fi
 
   [ "$NEW" ] && set -- -New "$@"
 
   cd "$(dirname "$0")"
   pwsh ./eopen.ps1 "$@"
-fi
+}
+
+open() {
+  if [ "$EDITOR" ]; then
+    open_editor "$@"
+  else
+    open_explorer "$@"
+  fi
+}
+
+open "$@"
