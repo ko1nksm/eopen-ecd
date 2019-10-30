7e4316547fbbf013a5d265c1c6b72378a1792950
Koichi Nakashima <koichi@nksm.name>
2019-10-30 20:49:00 +0900

    ecd.ps1: Improves handling for unicode paths

--- pwsh/ecd.ps1
+++ pwsh/ecd.ps1
@@ -1,6 +1,17 @@
-
-$encode = [Console]::OutputEncoding
-[Console]::OutputEncoding = [Text.Encoding]::UTF8
 $ewd = "$PSScriptRoot\ewd.ps1"
-cd (powershell -NoProfile -ExecutionPolicy Unrestricted $ewd).TrimEnd("`r?`n")
-[Console]::OutputEncoding = $encode
+$path = (powershell -NoProfile -ExecutionPolicy Unrestricted $ewd)
+$dir = [System.IO.Path]::GetDirectoryName($path)
+$file = [System.IO.Path]::GetFileName($path)
+$dirs = [System.IO.Directory]::GetDirectories($dir, $file)
+if ($dirs.length -eq 1) {
+  cd $path.TrimEnd("`r?`n")
+} else {
+  # Fallback when matching multiple unicode paths.
+  # Characters that are invalid in the current code page are replaced with '?'.
+  # Therefore, multiple folders may match. So switch the code page temporary.
+  $encode = [Console]::OutputEncoding
+  [Console]::OutputEncoding = [Text.Encoding]::UTF8
+  $path = (powershell -NoProfile -ExecutionPolicy Unrestricted $ewd)
+  cd $path.TrimEnd("`r?`n")
+  [Console]::OutputEncoding = $encode
+}
