107a8bd0951537509670f3cdfe57f9fb0d6f0def
Koichi Nakashima <koichi@nksm.name>
2019-11-05 20:53:03 +0900

    wslpath workaround

--- wsl/ewd.sh
+++ wsl/ewd.sh
@@ -2,6 +2,22 @@
 
 set -eu
 
+list() {
+  set --
+  i=1
+  while [ $i -lt 32 ]; do
+    set -- "$@" 128 $((i + 128)) "$i"
+    i=$((i+1))
+  done
+  set -- "$@" 128 162 34  128 170 42  128 186 58  128 188 60
+  set -- "$@" 128 190 62  128 191 63  129 156 92  129 188 124
+  printf 's!\xEF\x%X\x%X!\x%X!g\n' "$@"
+}
+
+wslpath() {
+  command wslpath "$@" | sed "$(list)"
+}
+
 abort() {
   [ $# -gt 0 ] && printf 'ewd: %s\n' "$1" >&2
   exit 1
@@ -15,7 +31,7 @@ ewd=$(
 case $ewd in ([A-Za-z]:* | \\\\*)
   if dest=$(wslpath -u "$ewd") 2>/dev/null; then
     printf '%s\n' "$dest"
-    exit
+    exit 0
   fi
 esac
 
--- wsl/md.sh
+++ wsl/md.sh
@@ -1,5 +1,29 @@
 #!/bin/sh
 
+set -eu
+
+list() {
+  set --
+  i=1
+  while [ $i -lt 32 ]; do
+    set -- "$@" 128 $((i + 128)) "$i"
+    i=$((i+1))
+  done
+  set -- "$@" 128 162 34  128 170 42  128 186 58  128 188 60
+  set -- "$@" 128 190 62  128 191 63  129 156 92  129 188 124
+  printf 's!\xEF\x%X\x%X!\x%X!g\n' "$@"
+}
+
+wslpath() {
+  command wslpath "$@" | sed "$(list)"
+}
+
+abort() {
+  [ $# -gt 0 ] && printf 'ewd: %s\n' "$1" >&2
+  echo false
+  exit 1
+}
+
 md() {
   while true; do
     case ${2:-} in
@@ -10,20 +34,14 @@ md() {
   printf "%s '%s'" "$1" "$2"
 }
 
-abort() {
-  [ $# -gt 0 ] && printf 'ewd: %s\n' "$1" >&2
-  echo false
-  exit 1
-}
-
 ewd=$(
   cd "$EOPEN_ROOT" || exit 1
   bin/ebridge.exe pwd
 ) || abort
 
 case $ewd in ([A-Za-z]:* | \\\\*)
-  if ewd=$(wslpath -u "$ewd" 2>/dev/null); then
-    md "$1" "$ewd"
+  if dest=$(wslpath -u "$ewd" 2>/dev/null); then
+    md "$1" "$dest"
     return 0
   fi
 esac
