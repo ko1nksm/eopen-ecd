ef3957a91ff0f3eb2d5224698c5675653f27c4d2
Koichi Nakashima <koichi@nksm.name>
2019-11-05 22:57:41 +0900

    ecd/epushd changes to behave like cd/pushd

--- wsl/md.sh
+++ wsl/md.sh
@@ -24,26 +24,69 @@ abort() {
   exit 1
 }
 
-md() {
+escape() {
   while true; do
     case ${2:-} in
-      *\'*) set -- "$1" "${2#*\'}" "${3:-}${2%%\'*}'\"'\"'" ;;
-      *) set -- "$1" "${3:-}${2:-}"; break ;;
+      *\'*) set -- "$1" "${2#*\'}" "${3:-}${2%%\'*}\"'\"" ;;
+      *) eval "$1=\${3:-}\${2:-}"; break ;;
     esac
   done
-  printf "%s '%s'" "$1" "$2"
 }
 
-ewd=$(
-  cd "$EOPEN_ROOT" || exit 1
-  bin/ebridge.exe pwd
-) || abort
+ewd() {
+  ewd=$(
+    cd "$EOPEN_ROOT" || exit 1
+    bin/ebridge.exe pwd
+  ) || abort
 
-case $ewd in ([A-Za-z]:* | \\\\*)
-  if dest=$(wslpath -u "$ewd" 2>/dev/null); then
-    md "$1" "$dest"
-    return 0
-  fi
-esac
+  case $ewd in ([A-Za-z]:* | \\\\*)
+    if dest=$(wslpath -u "$ewd" 2>/dev/null); then
+      printf '%s' "$dest"
+      return 0
+    fi
+  esac
 
-abort "Unable to move to '$ewd'"
+  abort "Unable to move to '$ewd'"
+}
+
+cmd=$1 stop='' at=''
+shift
+
+for param; do
+  case ${stop:+-}$param in
+    -* | +*) set -- "$@" "$param" ;;
+    *)
+      case $param in
+        "~~" | "~~/"*) # Windows home
+          whome=''
+          # param="$whome${param#~~}"
+          ;;
+        [A-Za-z]:* | \\\\*) # Windowd Path
+          ewd=$(ewd) || abort
+          param=$ewd
+          ;;
+        "@" | "@/"*) # Current Exploler Location
+          ewd=$(ewd) || abort
+          param="$ewd${param#@}"
+          [ "${param#@}" ] || at=1
+          ;;
+      esac
+      set -- "$@" "$param"
+      stop=1
+  esac
+  shift
+done
+
+for param; do
+  escape param "$param"
+  set -- "$@" "$param"
+  shift
+done
+
+set -- "$cmd" "$@"
+printf "'%s' " "$@"
+
+if [ ! "$at" ]; then
+  escape eopen "${0%/*}/eopen.sh"
+  printf "&& sh '%s' -g ." "$eopen"
+fi
