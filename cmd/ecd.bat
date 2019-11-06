514c8cd22809e378d3031673423140bb8f2314db
Koichi Nakashima <koichi@nksm.name>
2019-11-07 01:15:20 +0900

    new ecd.bat

--- cmd/ecd.bat
+++ cmd/ecd.bat
@@ -2,21 +2,61 @@
 
 setlocal
 set ebridge="%~dp0..\bin\ebridge.exe"
-for /f "usebackq tokens=*" %%i IN (`"%ebridge%" pwd auto`) DO set dir=%%i
-if errorlevel 0 exit /b 1
+set at=no
+call :path %*
 
-set /a count=0
-for /d %%i in ("%dir%") do set /a count=count+1
+cd /D %dest%
+if errorlevel 1 exit /b 1
 
-if "%count%" == "1" (
-  endlocal & cd "%dir%" & cd
+set pwd=%cd%
+if %pwd:~-1,1% == \ set pwd=%pwd%\
+if %at% == no "%ebridge%" open "%pwd%" b
+
+endlocal & cd /D %dest%
+
+exit /b
+
+:path
+  set dest=%*
+  if not defined dest set dest=%USERPROFILE% && exit /b
+
+  if %dest%%dest% == @@ set at=yes && call :ewd && exit /b
+  set pre=%dest:~0,2%
+  if %pre%%pre% == @\@\ call :ewd && exit /b
+  if %pre%%pre% == @/@/ call :ewd && exit /b
+  set pre=%dest:~0,3%
+  if %pre%%pre% == @"/@"/ call :ewd && exit /b
+  if %pre%%pre% == @"\@"\ call :ewd && exit /b
+
+  if %dest%%dest% == ~~ set at=yes && call :home && exit /b
+  set pre=%dest:~0,2%
+  if %pre%%pre% == ~\~\ call :home && exit /b
+  if %pre%%pre% == ~/~/ call :home && exit /b
+  set pre=%dest:~0,3%
+  if %pre%%pre% == ~"/~"/ call :home && exit /b
+  if %pre%%pre% == ~"\~"\ call :home && exit /b
+
   exit /b
-)
 
-rem Fallback when matching multiple unicode paths.
-rem Characters that are invalid in the current code page are replaced with '?'.
-rem Therefore, multiple folders may match. So switch the code page temporary.
-for /f "usebackq tokens=*" %%i IN (`"%ebridge%" chcp 65001`) DO set cp=%%i
-for /f "usebackq tokens=*" %%i IN (`"%ebridge%" pwd`) DO set dir=%%i
-"%ebridge%" chcp %cp% > NUL
-endlocal & cd %dir% & cd
+:ewd
+  for /f "usebackq tokens=*" %%i IN (`"%ebridge%" pwd auto`) do set pwd=%%i
+  if errorlevel 1 exit /b 1
+  set dir="%pwd%"%dest:~1%
+
+  rem set /a count=0
+  rem for /d %%i in ("%dir%") do set /a count=count+1
+
+  if "%count%" == "1" set dest=%dir% && exit /b
+
+  rem Fallback when matching multiple unicode paths.
+  rem Characters that are invalid in the current code page are replaced with '?'.
+  rem Therefore, multiple folders may match. So switch the code page temporary.
+  for /f "usebackq tokens=*" %%i IN (`"%ebridge%" chcp 65001`) DO set cp=%%i
+  for /f "usebackq tokens=*" %%i IN (`"%ebridge%" pwd`) DO set pwd=%%i
+  "%ebridge%" chcp %cp% > NUL
+  set dest="%pwd%"%dest:~1%
+  exit /b
+
+:home
+  set dest="%USERPROFILE%"%dest:~1%
+  exit /b
