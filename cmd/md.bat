cf779c0e8baf338f3ea30e36e3d908f5c825d20a
Koichi Nakashima <koichi@nksm.name>
2019-11-07 21:22:32 +0900

    Fix md.bat

--- cmd/md.bat
+++ cmd/md.bat
@@ -2,16 +2,17 @@
 
 setlocal
 set ebridge="%~dp0..\bin\ebridge.exe"
-set at=no
+set skip=no
 
 call :path %*
 if errorlevel 1 exit /b 1
 
 %cmd% %dest%
+if errorlevel 1 exit /b 1
 
 set pwd=%cd%
 if %pwd:~-1,1% == \ set pwd=%pwd%\
-if %at% == no "%ebridge%" open "%pwd%" b
+if %skip% == no "%ebridge%" open "%pwd%" b
 if errorlevel 1 exit /b 1
 
 endlocal & cd /D %cd%
@@ -22,21 +23,13 @@ exit /b
   set dest=%*
   if not defined dest set dest=%USERPROFILE% && exit /b
 
-  if %dest%%dest% == @@ set at=yes && call :ewd && exit /b
+  if %dest%%dest% == :: set skip=yes && call :ewd && exit /b
   set pre=%dest:~0,2%
-  if %pre%%pre% == @\@\ call :ewd && exit /b
-  if %pre%%pre% == @/@/ call :ewd && exit /b
+  set pre=%pre:/=\%
+  if %pre%%pre% == :\:\ call :ewd && exit /b
   set pre=%dest:~0,3%
-  if %pre%%pre% == @"/@"/ call :ewd && exit /b
-  if %pre%%pre% == @"\@"\ call :ewd && exit /b
-
-  if %dest%%dest% == ~~ set at=yes && call :home && exit /b
-  set pre=%dest:~0,2%
-  if %pre%%pre% == ~\~\ call :home && exit /b
-  if %pre%%pre% == ~/~/ call :home && exit /b
-  set pre=%dest:~0,3%
-  if %pre%%pre% == ~"/~"/ call :home && exit /b
-  if %pre%%pre% == ~"\~"\ call :home && exit /b
+  set pre=%pre:/=\%
+  if %pre%%pre% == :"\:"\ call :ewd && exit /b
 
   exit /b
 
@@ -47,6 +40,8 @@ exit /b
 
   set dir="%pwd%"%dest:~1%
 
+  set /a count=0
+  for /d %%i in ("%dir%") do set /a count=count+1
   if "%count%" == "1" set dest=%dir% && exit /b
 
   rem Fallback when matching multiple unicode paths.
@@ -56,8 +51,4 @@ exit /b
   for /f "usebackq tokens=*" %%i IN (`"%ebridge%" pwd`) DO set pwd=%%i
   "%ebridge%" chcp %cp% > NUL
   set dest="%pwd%"%dest:~1%
-  exit /b
-
-:home
-  set dest="%USERPROFILE%"%dest:~1%
   exit /b
