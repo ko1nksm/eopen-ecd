24872e92248c750c992d11e7025fb8c0131796d4
Koichi Nakashima <koichi@nksm.name>
2019-11-08 21:31:01 +0900

    init.sh: Prevent error when using quote in EOPEN_ROOT

--- init.sh
+++ init.sh
@@ -12,40 +12,51 @@ if [ ! -x "bin/ebridge.exe" ]; then
   exit 1
 fi
 
+escape() {
+  while true; do
+    case ${2:-} in
+      *\'*) set -- "$1" "${2#*\'}" "${3:-}${2%%\'*}'\"'\"'" ;;
+      *) eval "$1=\${3:-}\${2:-}"; break ;;
+    esac
+  done
+}
+
+escape BASE "$BASE"
+
 case ${1:-sh} in
 
 (sh) cat<<HERE
-export EOPEN_ROOT="$BASE"
-alias eopen='sh "$BASE/wsl/eopen.sh"'
-alias eclose='sh "$BASE/wsl/eclose.sh"'
-alias ewd='sh "$BASE/wsl/ewd.sh"'
-alias ecd='. "$BASE/wsl/ecd.sh"'
+export EOPEN_ROOT='$BASE'
+alias eopen='sh "\$EOPEN_ROOT/wsl/eopen.sh"'
+alias eclose='sh "\$EOPEN_ROOT/wsl/eclose.sh"'
+alias ewd='sh "\$EOPEN_ROOT/wsl/ewd.sh"'
+alias ecd='. "\$EOPEN_ROOT/wsl/ecd.sh"'
 if type pushd >/dev/null 2>&1; then
-  alias epushd='. "$BASE/wsl/epushd.sh"'
-  alias epopd='. "$BASE/wsl/epopd.sh"'
+  alias epushd='. "\$EOPEN_ROOT/wsl/epushd.sh"'
+  alias epopd='. "\$EOPEN_ROOT/wsl/epopd.sh"'
 fi
 HERE
 ;;
 
 (tcsh) cat<<HERE
-set EOPEN_ROOT="$BASE";
-alias eopen 'sh "$BASE/wsl/eopen.sh"';
-alias eclose 'sh "$BASE/wsl/eclose.sh"';
-alias ewd 'sh "$BASE/wsl/ewd.sh"';
-alias ecd 'source "$BASE/wsl/ecd.tcsh"';
-alias epushd 'source "$BASE/wsl/epushd.tcsh"';
-alias epopd 'source "$BASE/wsl/epopd.tcsh"';
+setenv EOPEN_ROOT '$BASE';
+alias eopen 'sh "\$EOPEN_ROOT/wsl/eopen.sh"';
+alias eclose 'sh "\$EOPEN_ROOT/wsl/eclose.sh"';
+alias ewd 'sh "\$EOPEN_ROOT/wsl/ewd.sh"';
+alias ecd 'source "\$EOPEN_ROOT/wsl/ecd.tcsh"';
+alias epushd 'source "\$EOPEN_ROOT/wsl/epushd.tcsh"';
+alias epopd 'source "\$EOPEN_ROOT/wsl/epopd.tcsh"';
 HERE
 ;;
 
 (fish) cat<<HERE
-set EOPEN_ROOT "$BASE";
-alias eopen='sh "$BASE/wsl/eopen.sh"';
-alias eclose='sh "$BASE/wsl/eclose.sh"';
-alias ewd='sh "$BASE/wsl/ewd.sh"';
-alias ecd='source "$BASE/wsl/ecd.fish"';
-alias epushd='source "$BASE/wsl/epushd.fish"';
-alias epopd='source "$BASE/wsl/epopd.fish"';
+set EOPEN_ROOT '$BASE';
+alias eopen='sh "\$EOPEN_ROOT/wsl/eopen.sh"';
+alias eclose='sh "\$EOPEN_ROOT/wsl/eclose.sh"';
+alias ewd='sh "\$EOPEN_ROOT/wsl/ewd.sh"';
+alias ecd='source "\$EOPEN_ROOT/wsl/ecd.fish"';
+alias epushd='source "\$EOPEN_ROOT/wsl/epushd.fish"';
+alias epopd='source "\$EOPEN_ROOT/wsl/epopd.fish"';
 HERE
 ;;
 
